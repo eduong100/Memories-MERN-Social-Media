3fb275227a26b58d7a1440a7255c17a8a335b32a
Ethan Duong <eduong100@gmail.com>
2022-06-10 19:46:56 -0700

    Initial Commit
    Set up express backend with routes. Set up MongoDB atlas. Set up REACT with redux for global access to axios get and post methods.  Set up new post form. Set up main page with retrieval of all posts.

---
+++ client/src/actions/posts.js
@@ -0,0 +1,20 @@
+import * as api from "../api";
+
+// Action Creators
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPosts(post);
+    dispatch({ type: "CREATE", payload: data });
+  } catch (err) {
+    console.log(err);
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+// The below CONNECTION_URL is hidden for security purposes
+import { CONNECTION_URL } from "./mongo_url.js";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
